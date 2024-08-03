0d57e1963ebc4b7e4305d5cff3acd7d245414d30
Grafals <itzsheesawan@gmail.com>
2024-08-03 16:58:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const typingElement = document.querySelector('.typing');
+const skills = ['HTML5', 'CSS6', 'JavaScript'];
+let skillIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < skills[skillIndex].length) {
+        typingElement.textContent += skills[skillIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, 150);
+    } else {
+        setTimeout(erase, 2000);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typingElement.textContent = skills[skillIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, 100);
+    } else {
+        skillIndex = (skillIndex + 1) % skills.length;
+        setTimeout(type, 500);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(type, 500);
+});
